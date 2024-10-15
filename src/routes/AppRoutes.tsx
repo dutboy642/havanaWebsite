5bb581eca2427560dc460194f8a4150f22c929ad
Tran Thanh Nguyen <trannguyen.06042002@gmail.comn>
2024-10-15 16:04:21 +0700

    fix routes

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -8,35 +8,10 @@ export const AppRouter = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />} >
-          <Route path='Home' element={<Homepage />} />
+          <Route index element={<Homepage />} />
+          {/* <Route path='Home' element={<Homepage />} /> */}
           <Route path='Shirt' element={<Shirt />} />
         </Route>
-        {/* <Route path="/auth" element={<Auth />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/bookinghotel" element={<BookingHotel />} />
-        <Route path="/bookcombo" element={<BookCombo />} />
-        <Route path="/detailtour" element={<DetailTour />} />
-        <Route path="/bookTicket" element={<BookTickets />} />
-        <Route path="/Chuyen-Di" element={<ChuyenDi />} />
-        <Route path="/booking" element={<Booking />} />
-        <Route path="/Home/" element={<Layout />}>
-          <Route path="news" element={<News />} />
-          <Route path="Intro" element={<Intro />} />
-        </Route>
-        <Route path="/Setting/" element={<Setting />}>
-          <Route index element={<PersonalInformationSetting />} />
-          <Route path="Options" element={<OptionsSetting />} />
-          <Route path="Safety" element={<Safety />} />
-          <Route path="Billing" element={<BillingSetting />} />
-          <Route path="Privacy" element={<PrivacySetting />} />
-          <Route path="Email" element={<EmailSetting />} />
-          <Route path="Companion" element={<CompanionSetting />} />
-        </Route>
-         <Route path="/Admin/" element={<AdminLayout />}>
-                    <Route path='Dashboard' element={<AdminDashboard />} />
-                    <Route path='ManageHotel' element={<ManageHotel />} />
-                    <Route path='ManageTicket' element={<ManageTicket />} />
-                </Route> */}
       </Routes>
     </BrowserRouter>
   );
