37cfc69840972b87cf257c4217556627c0871037
Tran Thanh Nguyen <trannguyen.06042002@gmail.comn>
2024-10-17 00:41:14 +0700

    tellmewhy

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -4,7 +4,6 @@ import Homepage from "../pages/Homepage";
 import Layout from "../pages/Layouts";
 import Set from "../pages/Set";
 import All from "../pages/All";
-import Stuff from "../pages/Stuff";
 import Sale from "../pages/Sale";
 import CheckOrderPage from "../pages/CheckOrder";
 import LoginForm from "../pages/Login";
@@ -26,7 +25,6 @@ export const AppRouter = () => {
           <Route path='Skirt' element={<Skirt />} />
           <Route path='Shirt' element={<Shirt />} />
           <Route path='All' element={<All />} />
-          <Route path='Stuff' element={<Stuff />} />
           <Route path='Sale' element={<Sale />} />
           <Route path='CheckOrderPage' element={<CheckOrderPage />} />
           <Route path='LoginForm' element={<LoginForm />} />
